136d91e147483d452da319682e8fd9c31b92e67f
ggandra <gandra.gabriel@hotmail.com>
2020-05-31 22:46:06 -0300

    envio das coordenadas

--- index.js
+++ index.js
@@ -9,11 +9,14 @@ io.on('connection', socket => {
     count ++;
     console.log(count);
     if (socket.handshake.query.match) {
-        console.log('socket connection');
+        
     }
+    socket.join(1);
     socket.on('updateLocation', data => {
         console.log(data);
-        socket.broadcast.emit('broadcast', 'hello everyone');
+        socket.to(1).emit('locationReceived', { coordinates: {
+            latitude: '-21.2121147', longitude: '-50.4170587,17z'
+        },})
     });
     socket.on('disconnect', data => {
         count --;
