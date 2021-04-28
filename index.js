12cf9779b7085daacd9671115ae29491e847317c
Gabriel Gandra <gandra.gabriel@hotmail.com>
2021-04-28 20:44:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,18 @@ const server = express();
 const serverListening = server.listen(3030);
 const io = require('socket.io')(serverListening);
 
-let count = 0;
 io.on('connection', socket => {
-    count ++;
-    console.log(count);
     if (socket.handshake.query.match) {
         console.log('hanshake')
     }
     socket.join(1);
     socket.on('updateLocation', data => {
-        console.log(data);
         socket.to(1).emit('locationReceived', { coordinates: {
             latitude: data.latitude, longitude: data.longitude, courier_id: data.id
         },})
     });
     socket.on('disconnect', data => {
-        count --;
-        console.log(count);
+        console.log('disconnected');
     });
  });
 
